a97b9a17b8ceb9b767cfc86ec854ba8798c9565f
abirbenaissa <ab.benaissa@esi-sba.dz>
2022-10-29 12:24:46 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+       
+    </head>
+    <body >
+       Hello, this is an empty page nothing impo
+    </body>
+</html>
